96ef479987977757b27c2e5929eec19b9debe0c4
Roma Kamenivskiy <jumji19992412@gmail.com>
2019-10-20 22:03:15 +0300

    Updates

---
+++ precache-manifest.f9459b9fe0193e178a10316e6406a14b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b748ef9ca86efe9e62e8649f996cf2b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "905be26cead8ccfa8997",
+    "url": "/portfolio/static/css/main.cb4bfb37.chunk.css"
+  },
+  {
+    "revision": "d778698d514102b5e10a",
+    "url": "/portfolio/static/js/2.e13079bb.chunk.js"
+  },
+  {
+    "revision": "905be26cead8ccfa8997",
+    "url": "/portfolio/static/js/main.e99fd47b.chunk.js"
+  },
+  {
+    "revision": "e6c07da49bdb817c2935",
+    "url": "/portfolio/static/js/runtime-main.dbe6b12d.js"
+  },
+  {
+    "revision": "f736bb9e96f643f49ad1e65f44c3c2b9",
+    "url": "/portfolio/static/media/name-bg.f736bb9e.jpg"
+  }
+]);
